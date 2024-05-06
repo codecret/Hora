ffae082ad9fafe1f3698be56f080ccef7cf9fa8b
codecret <malekkhodary@hotmail.com>
2024-05-06 05:11:17 +0300

    feat: :sparkles: preview data in dashboard, styling improvment

--- client/src/components/AppointmentItem/AppointmentItem.jsx
+++ client/src/components/AppointmentItem/AppointmentItem.jsx
@@ -1,22 +1,59 @@
 import "./AppointmentItem.css";
 import { Icon } from "@iconify/react";
+import dayjs from "dayjs";
 
-const AppointmentItem = ({ title, date, time, participant, description }) => {
+const AppointmentItem = ({
+  title,
+  startDate,
+  startTime,
+  endTime,
+  participants,
+  description,
+}) => {
+  const emptyStartDate = startDate || "";
+  const emptyStartTime = startTime || "";
+  const emptyEndTime = endTime || "";
+
+  const startTimeFormatted = new Date(`${emptyStartTime}`).toLocaleTimeString(
+    [],
+    {
+      hour: "numeric",
+      minute: "2-digit",
+      hour12: true,
+    }
+  );
+
+  const endTimeFormatted = new Date(`${emptyEndTime}`).toLocaleTimeString([], {
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+  });
+
+  const formattedStartDate = dayjs(emptyStartDate).format("dddd, MMM D");
+
   return (
     <div className="appointment-card">
       <div className="card-leftside">
         <h2 className="appointment-title">{title}</h2>
         <div className="appointment-date">
           <Icon icon="majesticons:calendar-line" width="1.2em" height="1.2em" />
-          <p>{date}</p>
+          <p>{startDate && formattedStartDate}</p>
         </div>
-        <div className="appointment-time">
-          <Icon icon="majesticons:clock-line" width="1.2em" height="1.2em" />
-          <p>{time}</p>
-        </div>
+        {startTime && (
+          <div className="appointment-time">
+            <Icon icon="majesticons:clock-line" width="1.2em" height="1.2em" />
+            <p>{`${startTime && startTimeFormatted} - ${
+              endTime && endTimeFormatted
+            }`}</p>
+          </div>
+        )}
       </div>
       <div className="card-rightside">
-        <p className="appointment-name">{participant}</p>
+        <p className="appointment-name">
+          {participants.length > 0
+            ? `${participants[0]} +${participants.length - 1} others.`
+            : participants[0]}
+        </p>
         <p className="appointment-description">{description}</p>
         <Icon
           className="arrow-icon"
--- client/src/pages/DashboardPage/DashboardPage.jsx
+++ client/src/pages/DashboardPage/DashboardPage.jsx
@@ -1,58 +1,26 @@
 import "./DashboardPage.css";
 import AppointmentItem from "../../components/AppointmentItem/AppointmentItem";
+import { useGetAppointments } from "../../hooks/useAppointments";
+import Loader from "../../components/Loader";
 
 const DashboardPage = () => {
-  const appointments = [
-    {
-      title: "Appointment 1",
-      date: "2024-04-30",
-      time: "10:00 AM",
-      participant: "John Doe",
-      description:
-        "Lorem ipsum, dolor sit amet consectetur atione facilis, at mollitia recusandae ullam. Error, earum magnam a est animi porro illo aliquid consequuntur?",
-    },
-    {
-      title: "Appointment 2",
-      date: "2024-05-05",
-      time: "2:30 PM",
-      participant: "Jane Smith",
-      description:
-        "Lorem ipsum, dolor sit amet consectetur atione facilis, at mollitia recusandae ullam. Error, earum magnam a est animi porro illo aliquid consequuntur?",
-    },
-    {
-      title: "Appointment 2",
-      date: "2024-05-05",
-      time: "2:30 PM",
-      participant: "Jane Smith",
-      description:
-        "Lorem ipsum, dolor sit amet consectetur atione facilis, at mollitia recusandae ullam. Error, earum magnam a est animi porro illo aliquid consequuntur?",
-    },
-    {
-      title: "Appointment 2",
-      date: "2024-05-05",
-      time: "2:30 PM",
-      participant: "Jane Smith",
-      description:
-        "Lorem ipsum, dolor sit amet consectetur atione facilis, at mollitia recusandae ullam. Error, earum magnam a est animi porro illo aliquid consequuntur?",
-    },
-    {
-      title: "Appointment 2",
-      date: "2024-05-05",
-      time: "2:30 PM",
-      participant: "Jane Smith",
-      description:
-        "Lorem ipsum, dolor sit amet consectetur atione facilis, at mollitia recusandae ullam. Error, earum magnam a est animi porro illo aliquid consequuntur?",
-    },
-  ];
-
+  const { data, isLoading, isFetching, isError } = useGetAppointments();
+  if (isLoading || isFetching) {
+    return <Loader />;
+  }
   return (
     <div className="two-column-div">
       <div className="left-column">
         <div className="appointment-list">
           <h2 className="appointment-list-heading">Upcoming Appointments</h2>
-          {appointments.map((appointment, index) => (
-            <AppointmentItem key={index} {...appointment} />
-          ))}
+          {data
+            ?.slice(0, 3)
+            .map(
+              (appointment, index) => (
+                console.log(appointment),
+                (<AppointmentItem key={index} {...appointment} />)
+              )
+            )}
         </div>
       </div>
 
